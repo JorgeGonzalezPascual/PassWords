7781d8833a8cd66cdd3dc134391a9259525b3edc
JorgeGonzalezPascual <61434203+JorgeGonzalezPascual@users.noreply.github.com>
2021-03-09 09:34:51 +0100

    Add press ENTER to show a password

--- src/Clases/VentanaPrincipal.java
+++ src/Clases/VentanaPrincipal.java
@@ -11,6 +11,8 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -238,8 +240,31 @@ public class VentanaPrincipal extends JFrame {
         taPassword = new JTextArea("Contraseña");
         taPassword.setEditable(false);
 
+        //Acccion al pulsar ENTER en la casilla Label        
+        tLabel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent ke) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent ke) {
+                //Al presionar la tencla ENTER
+                if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (!tLabel.getText().equals("")) {
+                        bVer();
+                    } else {
+                        pantalla.setText("Escribe la ETIQUETA");
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent ke) {
+            }
+        });
+
         //Scroll Pane
-        scrollPane = new JScrollPane(pantalla, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+        scrollPane = new JScrollPane(pantalla, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVisible(true);
 
